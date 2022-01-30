480529c74cafa8ca8eb70a84f0ff54ad4eaf873a
Marek Kabala <marekkabala1@gmail.com>
2022-01-30 20:06:04 +0000

    adding maps

--- dist/scrypt.js
+++ dist/scrypt.js
@@ -2,8 +2,17 @@
 const hamburgerWrapper = document.querySelector("#hamburger-wrapper");
 const hamburger = document.querySelectorAll("#hamburger-wrapper div");
 const navWrapper = document.querySelector("#nav-wrapper");
+const body = document.querySelector("body");
 
-hamburgerWrapper.addEventListener("click", () => {
+hamburgerWrapper.addEventListener("click", (e) => {
   navWrapper.classList.toggle("nav-active");
   hamburgerWrapper.classList.toggle("active");
+  window.addEventListener("click", function (e) {});
 });
+// hamburger.forEach((item) =>
+//   item.addEventListener("click", function (e) {
+//     navWrapper.classList.toggle("nav-active");
+//     hamburgerWrapper.classList.toggle("active");
+//     console.log(this);
+//   })
+// );
