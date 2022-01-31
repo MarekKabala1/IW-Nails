77978093362718303de5b35ac90b525d223e25f1
Marek Kabala <marekkabala1@gmail.com>
2022-01-31 06:11:58 +0000

    fixing small error in footer

--- dist/scrypt.js
+++ dist/scrypt.js
@@ -2,17 +2,23 @@
 const hamburgerWrapper = document.querySelector("#hamburger-wrapper");
 const hamburger = document.querySelectorAll("#hamburger-wrapper div");
 const navWrapper = document.querySelector("#nav-wrapper");
-const body = document.querySelector("body");
 
-hamburgerWrapper.addEventListener("click", (e) => {
+hamburgerWrapper.addEventListener("click", () => {
   navWrapper.classList.toggle("nav-active");
   hamburgerWrapper.classList.toggle("active");
-  window.addEventListener("click", function (e) {});
 });
-// hamburger.forEach((item) =>
-//   item.addEventListener("click", function (e) {
-//     navWrapper.classList.toggle("nav-active");
-//     hamburgerWrapper.classList.toggle("active");
-//     console.log(this);
-//   })
-// );
+
+document.addEventListener("click", (e) => {
+  if (e.target !== hamburgerWrapper) {
+    navWrapper.classList.remove("nav-active");
+    hamburgerWrapper.classList.remove("active");
+  }
+});
+
+const french = document.querySelector(".section-three_french");
+
+french.addEventListener("click", (e) => {
+  // const div = document.createElement("div");
+  // french.appendChild(div);
+  console.log("click", e.target);
+});
