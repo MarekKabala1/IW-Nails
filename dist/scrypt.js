9dedd12febac396296b1edd23e2cb01f6e25927f
Marek Kabala <marekkabala1@gmail.com>
2022-02-07 06:19:29 +0000

    adding phone number

--- dist/scrypt.js
+++ dist/scrypt.js
@@ -1,48 +1,59 @@
 // hamburger menu
-const hamburgerWrapper = document.querySelector("#hamburger-wrapper");
-const hamburger = document.querySelectorAll("#hamburger-wrapper div");
-const navWrapper = document.querySelector("#nav-wrapper");
+const manu = () => {
+  const hamburgerWrapper = document.querySelector("#hamburger-wrapper");
+  const hamburger = document.querySelectorAll("#hamburger-wrapper div");
+  const navWrapper = document.querySelector("#nav-wrapper");
 
-hamburgerWrapper.addEventListener("click", () => {
-  navWrapper.classList.toggle("nav-active");
-  hamburgerWrapper.classList.toggle("active");
-});
+  hamburgerWrapper.addEventListener("click", () => {
+    navWrapper.classList.toggle("nav-active");
+    hamburgerWrapper.classList.toggle("active");
+  });
 
-document.addEventListener("click", (e) => {
-  if (e.target !== hamburgerWrapper) {
-    navWrapper.classList.remove("nav-active");
-    hamburgerWrapper.classList.remove("active");
-  }
-});
+  document.addEventListener("click", (e) => {
+    if (e.target !== hamburgerWrapper) {
+      navWrapper.classList.remove("nav-active");
+      hamburgerWrapper.classList.remove("active");
+    }
+  });
+};
+manu();
 
-// My service popup menu
-const popupFrench = document.querySelectorAll(".section-three_li");
-const popupClassic = document.querySelector(".classic-photo");
-const popupPhotoDiv = document.querySelector(".section-three-li_photo");
-const mainConteiner = document.querySelector(".main-conteiner");
+// My service popup photos,removing and adding background
+const popupPhotos = () => {
+  const popupFrench = document.querySelectorAll(".section-three_li");
+  const popupClassic = document.querySelector(".classic-photo");
+  const popupPhotoDiv = document.querySelector(".section-three-li_photo");
+  const mainConteiner = document.querySelector(".main-conteiner");
+  const sections = document.querySelectorAll("section");
 
-popupFrench.forEach((popup, index) => {
-  popup.addEventListener("click", (e) => {
-    let imgArr = [index];
+  popupFrench.forEach((popup, index) => {
+    popup.addEventListener("click", () => {
+      let imgArr = [index];
 
-    // prettier-ignore
-    popupPhotoDiv.innerHTML =
+      // prettier-ignore
+      popupPhotoDiv.innerHTML =
     `<img src="img&svg/service${imgArr}.jpeg">`;
 
-    document.addEventListener("click", (e) => {
-      if (e.target === popup) {
-        popupPhotoDiv.style.display = "flex";
-        popupPhotoDiv.style.animation = "scaleAnimation 400ms ease-in";
-        mainConteiner.classList.remove("main-conteiner");
-        mainConteiner.classList.add("overlay");
-      } else {
-        popupPhotoDiv.style.display = "none";
-        popupPhotoDiv.style.animation = "";
-        mainConteiner.classList.remove("overlay");
-        mainConteiner.classList.add("main-conteiner");
-      }
-      // console.log(e.target);
+      document.addEventListener("click", (e) => {
+        sections.forEach((section) => {
+          if (e.target === popup) {
+            popupPhotoDiv.style.display = "flex";
+            popupPhotoDiv.style.animation = "scaleAnimation 400ms ease-in";
+            mainConteiner.classList.remove("main-conteiner");
+            mainConteiner.classList.add("overlay");
+            section.style.background = "none";
+            // section.style.background = "none";
+          } else {
+            popupPhotoDiv.style.display = "none";
+            popupPhotoDiv.style.animation = "";
+            mainConteiner.classList.remove("overlay");
+            mainConteiner.classList.add("main-conteiner");
+            section.style.background = "";
+          }
+          // console.log(e.target);
+        });
+      });
     });
   });
-});
-// console.log(index, popupPhotoDiv, imgArr);
+};
+popupPhotos();
